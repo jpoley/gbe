e584e5df07aba49ec5d8813285fc97dcbf5ccc33
jpoley <jason.poley@gmail.com>
2020-05-25 21:56:50 -0400

    partial

---
+++ switch/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	i := 2
+	fmt.Print("Write ", i, " as ")
+	switch i {
+	case 1:
+		fmt.Println(" one ")
+	case 2:
+		fmt.Println(" two ")
+	case 3:
+		fmt.Println(" three ")
+	}
+	switch time.Now().Weekday() {
+	case time.Saturday, time.Sunday:
+		fmt.Println(" its the weekend")
+	default:
+		fmt.Println(" its a weekday")
+	}
+
+	t := time.Now()
+	switch {
+	case t.Hour() < 12:
+		fmt.Println(" its before noon")
+	default:
+		fmt.Println(" its after noon")
+	}
+
+	whatAmI := func(i interface{}) {
+		switch t := i.(type) {
+		case bool:
+			fmt.Println(" i'm a bool")
+		case int:
+			fmt.Println(" I'm an int")
+		default:
+			fmt.Printf("Don't know what type %T\n", t)
+		}
+	}
+	whatAmI(true)
+	whatAmI(1)
+	whatAmI("hey")
+
+}
---
+++ tickers/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	ticker := time.NewTicker(500 * time.Microsecond)
+	done := make(chan bool)
+
+	go func() {
+		for {
+			select {
+			case <-done:
+				return
+			case t := <-ticker.C:
+				fmt.Println("tick at", t)
+			}
+
+		}
+	}()
+
+	time.Sleep(1600 * time.Microsecond)
+	ticker.Stop()
+	done <- true
+	fmt.Println("Ticker stopped")
+}
